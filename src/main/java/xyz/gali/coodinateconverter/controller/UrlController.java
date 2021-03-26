22162dd9a8968d9be6f1922b4a50da8125819f39
Admin <flashowingy@gmail.com>
2021-03-26 16:31:10 +0800

    添加控制器层解析地址

--- src/main/java/xyz/gali/coodinateconverter/controller/UrlController.java
+++ src/main/java/xyz/gali/coodinateconverter/controller/UrlController.java
@@ -1,12 +1,14 @@
 package xyz.gali.coodinateconverter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class UrlController {
 
-
-    public void pickPositionMap(){
-
+    @RequestMapping("pick")
+    public String pickPositionMap(){
+        return "./example/pickPositionOnMap.html";
     }
 }
