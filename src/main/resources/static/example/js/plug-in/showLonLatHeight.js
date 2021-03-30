0dd31a2276d26529963e00af9083c885e44f0c75
Admin <flashowingy@gmail.com>
2021-03-30 18:59:33 +0800

    获取地形高度

--- src/main/resources/static/example/js/plug-in/showLonLatHeight.js
+++ src/main/resources/static/example/js/plug-in/showLonLatHeight.js
@@ -38,12 +38,14 @@ showLocation.prototype = {
                 //根据射线和场景求出在球面中的笛卡尔坐标
                 let position1 = viewer.scene.globe.pick(ray, viewer.scene);
                 //获取该浏览器坐标的顶部数据
-                let feature = viewer.scene.pick(movement.endPosition);
-                if (feature == undefined && position1) {
+                // let feature = viewer.scene.pick(movement.endPosition);
+                // console.log(feature)
+                // if (feature == undefined && position1) {
+                if (position1) {
                     let cartographic1 = Cesium.Ellipsoid.WGS84.cartesianToCartographic(position1);
                     lon = Cesium.Math.toDegrees(cartographic1.longitude);
                     lat = Cesium.Math.toDegrees(cartographic1.latitude);
-                    MouseHeight = 0;
+                    MouseHeight = cartographic1.height;
                 } else {
                     let cartesian = viewer.scene.pickPosition(movement.endPosition);
                     if (Cesium.defined(cartesian)) {
@@ -57,7 +59,7 @@ showLocation.prototype = {
                 // 获取相机的高度
                 cameraHeight = Math.ceil(viewer.camera.positionCartographic.height);
                 try {
-                    _this.coordinatesDiv.innerHTML = "<span id='cd_label' style='font-size:13px;text-align:center;font-family:微软雅黑;color:#edffff;'>视角高度:" + cameraHeight + "米&nbsp;&nbsp;&nbsp;&nbsp;模型高度:" + MouseHeight.toFixed(2) + "米&nbsp;&nbsp;&nbsp;&nbsp;经度：" + lon.toFixed(6) + "&nbsp;&nbsp;纬度：" + lat.toFixed(6) + "</span>";
+                    _this.coordinatesDiv.innerHTML = "<span id='cd_label' style='font-size:13px;text-align:center;font-family:微软雅黑;color:#edffff;'>视角高度:" + cameraHeight + "米&nbsp;&nbsp;&nbsp;&nbsp;地形高度:" + MouseHeight.toFixed(2) + "米&nbsp;&nbsp;&nbsp;&nbsp;经度：" + lon.toFixed(6) + "&nbsp;&nbsp;纬度：" + lat.toFixed(6) + "</span>";
                 } catch (e) {
 
                 }
